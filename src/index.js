d5d77c6e868b128470c3a10bc96dbf456098e318
Laurel McLean <laurel.mclean@live.ca>
2023-08-16 17:05:08 -0600

    completed remove extra spaces

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// makes the first character of a given string uppercase.
 function capitalize(str) {
     const firstLetter = str[0].toUpperCase();
     const remainder = str.slice(1);
@@ -5,11 +6,13 @@ function capitalize(str) {
     return capitalized;
 }
 
+// makes all characters uppercase.
 function allCaps(str) {
     const capitalized = str.toUpperCase();
     return capitalized;
 }
 
+// makes the first character of each word uppercase.
 function capitalizeWords(str) {
     const array = str.split(' ');
     const mappedArray = array.map((word) => capitalize(word));
@@ -17,9 +20,19 @@ function capitalizeWords(str) {
     return result;
 }
 
+// Removes all spaces from the beginning and end of a String along with any extra spaces in the middle
+function removeExtraSpaces(str) {
+    const trimmedString = str.trim();
+    const splitString = trimmedString.split(' ');
+    const filtered = splitString.filter((word) => word != '');
+    const result = filtered.join(' ');
+    return result;
+}
+
 console.log(capitalize('hi laurel'));
 console.log(allCaps('hi laurel'));
 console.log(capitalizeWords('hi laurel'));
+console.log(removeExtraSpaces(' hi    laurel '));
 
 module.exports = {
     capitalize,
