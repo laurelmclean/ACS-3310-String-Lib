34c17e7fa8e5f0927efd5040948b707635d7772e
Laurel McLean <laurel.mclean@live.ca>
2023-08-16 12:58:53 -0600

    completed capitalize words

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 function capitalize(str) {
-    const firstLetter = str[0].toUpperCase()
-    const remainder = str.slice(1)
-    const capitalized = firstLetter.concat(remainder)
-    return capitalized
+    const firstLetter = str[0].toUpperCase();
+    const remainder = str.slice(1);
+    const capitalized = firstLetter.concat(remainder);
+    return capitalized;
 }
 
 function allCaps(str) {
-    const capitalized = str.toUpperCase()
-    return capitalized
+    const capitalized = str.toUpperCase();
+    return capitalized;
 }
 
-console.log(capitalize('hi laurel'))
-console.log(allCaps('hi laurel'))
+function capitalizeWords(str) {
+    const array = str.split(' ');
+    const mappedArray = array.map((word) => capitalize(word));
+    const result = mappedArray.join(' ');
+    return result;
+}
+
+console.log(capitalize('hi laurel'));
+console.log(allCaps('hi laurel'));
+console.log(capitalizeWords('hi laurel'));
 
 module.exports = {
-    capitalize
+    capitalize,
+    allCaps,
+    capitalizeWords
 };
